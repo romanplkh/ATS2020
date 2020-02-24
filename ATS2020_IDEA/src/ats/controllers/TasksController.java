28a614dcb17620836a560565213fda96011b7ae1
Olena Stepanova <86sthelen@gmail.com>
2020-02-24 18:29:17 -0400

    added TasksController, part of task page UI

---
+++ ATS2020_IDEA/src/ats/controllers/TasksController.java
@@ -0,0 +1,25 @@
+package ats.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Olena Stepanova 
+ */
+
+@WebServlet(name = "TasksController")
+public class TasksController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
